881750c13131c9bc8fcaf5b5072aed7fc02dbdec
martinbee <martin@poeticsystems.com>
2016-07-19 12:50:14 -0500

    try different csv generator

--- index.js
+++ index.js
@@ -3,18 +3,23 @@ var AWS = require('aws-sdk');
 var Papa = require('babyparse');
 var jsonData = require('./jsonData.js');
 var fs = require('fs');
+var json2csv = require('json2csv');
 
-var csv = Papa.unparse(JSON.stringify(jsonData.data));
+try {
+  var csv = json2csv({ data: jsonData.data });
+} catch (err) {
+  console.error(err);
+}
 
 var s3 = new AWS.S3({accessKeyId: process.env.ACCESS_KEY_ID, secretAccessKey: process.env.SECRET_ACCESS_KEY});
-var params = {Bucket: process.env.BUCKET_NAME, Key: 'test-csv', Body: csv};
-var getParams = {Bucket: process.env.BUCKET_NAME, Key: 'test-csv'};
+var params = {Bucket: process.env.BUCKET_NAME, Key: 'test-csv-2', Body: csv};
+var getParams = {Bucket: process.env.BUCKET_NAME, Key: 'test-csv-2'};
 
-// s3.getObject(getParams, function(err, data) {
-  // var wstream = fs.createWriteStream('./test-csv.csv');
-  // wstream.write(data.Body);
-// });
-
-s3.upload(params, {}, function(err, data) {
-  console.log(err, data);
+s3.getObject(getParams, function(err, data) {
+  var wstream = fs.createWriteStream('./test-csv-2.csv');
+  wstream.write(data.Body);
 });
+
+// s3.upload(params, {}, function(err, data) {
+  // console.log(err, data);
+// });
